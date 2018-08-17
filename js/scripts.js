1a5096a9f023d85a0563df04081f555648028985
Hingyi Khong <hingyi@gmail.com>
2018-08-17 14:40:36 -0700

    check indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,9 @@
 
 var beepBoop = function(userInput) {
 var answer = [];
-
+// loop goes through from 0 to user number, adds value by 1
   for (var i = 0; i <= userInput; i++) {
+// Turns input into string, checks for input divisible by "3", and numbers containing "0" or "1".
     if  (i.toString() > 0 && i.toString() % 3 === 0) {
       answer.push (" I'm sorry, Dave. I'm afraid I can't do that.");
     } else if (i.toString().includes("0")) {
@@ -12,44 +13,27 @@ var answer = [];
       answer.push(" Boop!");
     } else {
       answer.push(i + "");
-    };
   };
-
-  console.log(answer)
-    return answer;
 };
-// var userInput = function(#inputUserForm) {
-// }
-//
-// function error(userInput) {
-//     return userInput % 1 === 0;
-//
-//     alert("Must input numbers");
-//     return false;)
-// }
-//$(document).ready(function()
+// console.log(answer)
+return answer;
+};
 
-// }
-// }
+
+
 //Front End User Interface
+
 $(document).ready(function() {
   $("form#userForm").submit(function(event) {
   event.preventDefault();
   var inputNumber = parseInt($("input#inputUserForm").val());
   var finalNumber = beepBoop(inputNumber);
-
-
-  if (isNaN(inputNumber)) {
-    alert("Error. Not a valid number, try again!");
-  }
-    $("#answer").show();
-    $("#answer").empty();
-
-    $("<li>" + finalNumber + "</li>").appendTo("#answer");
-    $("form#userForm")[0].reset();
+    if (isNaN(inputNumber)) {
+      alert("Error. Not a valid number, try again!");
+    }
+      $("#answer").show();
+      $("#answer").empty();
+      $("<li>" + finalNumber + "</li>").appendTo("#answer");
+      $("form#userForm")[0].reset();
   });
 });
-
-  // var userInput = parseInt($("input#userInputForm").val());
-    // if (isNaN(userInput) || userInput < 0) {
-    //   alert("Error. Not a valid number, try again!")
