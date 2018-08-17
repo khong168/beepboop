5c0bfc0936427158461e1aa8531af7a91b8effd2
Hingyi Khong <hingyi@gmail.com>
2018-08-17 11:02:23 -0700

    add js back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 // Business Logic- Back End
+
+var beepBoop = function(userInput) {
+  console.log("muffin")
+var output = [];
+  for (var i = 0; i <= userInput; i++) {
+    if  (i.toString().includes("0")) {
+      output.push(" Beep!");
+    } else if (i.tostring().includes("1")) {
+      output.push(" Boop!");
+    }else if (i % 3 === 0) {
+      output.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+    } else {
+      output.push(" " + i);
+    }
+  }
+  return outcome;
+}
 //
-// function beepBoop(inputNumber) {
-// var output = [];
-//
-// for  (var 1 = 0; i <= inputNumber; i++) {
-//   var numbers = i.toString().split("");
 //   if (i % 3 === 0 && i> 0) {
 //     output.push();
 //   }
@@ -34,20 +46,22 @@
 //     alert("Must input numbers");
 //     return false;)
 // }
-//
-//
+//$(document).ready(function()
+
 // }
 // }
 //Front End User Interface
 $(document).ready(function() {
   $("form#userForm").submit(function(event) {
   event.preventDefault();
-  var userInput = parseInt($("input#userInputForm") .val());
-    if (isNAN(userInput) || userInput < 0) {
-      alert("Not a valid number, try again!")
-    } else {
-      
-    }
+
+  var userInput = parseInt($("input#userInputForm").val());
+  // var Result = output(inputNumber)
+    // if (isNaN(userInput) || userInput < 0) {
+    //   alert("Error. Not a valid number, try again!")
+
+    // }
+});
 });
 
 
