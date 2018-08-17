5777c54e87fdfbdea8706388a578f8e201a385cb
Hingyi Khong <hingyi@gmail.com>
2018-08-17 10:34:27 -0700

    add js front end logic

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+// Business Logic- Back End
+//
+// function beepBoop(inputNumber) {
+// var output = [];
+//
+// for  (var 1 = 0; i <= inputNumber; i++) {
+//   var numbers = i.toString().split("");
+//   if (i % 3 === 0 && i> 0) {
+//     output.push();
+//   }
+// }
+
+// $(function() {
+//
+// })
+//
+// function error(userInput) {
+//   var userInput = (parseInt(#inputUserForm).val());
+//     if ( parseFloat(userInput) !== parseInt(userInput) || parseInt(input) < 1 ){
+//     return "Error. Invalid Input";
+//     }
+//   }
+
+
+
+
+
+// var userInput = function(#inputUserForm) {
+// }
+//
+// function error(userInput) {
+//     return userInput % 1 === 0;
+//
+//     alert("Must input numbers");
+//     return false;)
+// }
+//
+//
+// }
+// }
+//Front End User Interface
+$(document).ready(function() {
+  $("form#userForm").submit(function(event) {
+  event.preventDefault();
+  var userInput = parseInt($("input#userInputForm") .val());
+    if (isNAN(userInput) || userInput < 0) {
+      alert("Not a valid number, try again!")
+    } else {
+      
+    }
+});
+
+
+//   var UserInput = parseInt($("input#userInputForm").val());
+//   });
+// });
+//     var output =
+//   }))
+//
+//
+//
+//
+//   function beepBoop(inputNumber) {
+// var output = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }
