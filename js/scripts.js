51bd98c92b091a1de0a600298a79ebf64eaa7bd5
Hingyi Khong <hingyi@gmail.com>
2018-08-17 13:07:06 -0700

    add script to clear results

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 
 var beepBoop = function(userInput) {
 var answer = [];
+
   for (var i = 0; i <= userInput; i++) {
     if  (i.toString().includes("0")) {
       answer.push(" Beep!");
@@ -36,10 +37,19 @@ $(document).ready(function() {
   event.preventDefault();
   var inputNumber = parseInt($("input#inputUserForm").val());
   var finalNumber = beepBoop(inputNumber);
+
+
+  if (isNaN(inputNumber)) {
+    alert("Error. Not a valid number, try again!");
+  }
+    $("#answer").hide();
     $("#answer").show();
+
     $("<li>" + finalNumber + "</li>").appendTo("#answer");
+    $("form#userForm")[0].reset();
   });
 });
+
   // var userInput = parseInt($("input#userInputForm").val());
     // if (isNaN(userInput) || userInput < 0) {
     //   alert("Error. Not a valid number, try again!")
