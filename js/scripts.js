5642b5005c17640f213a9811dbec6424e6baf119
hingyi khong <hingyi@gmail.com>
2018-08-23 11:36:14 -0700

    fix indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,40 @@
 // Business Logic- Back End
 
 var beepBoop = function(userInput) {
-var answer = [];
-// loop goes through from 0 to user number, adds value by 1 until limit
+  var answer = [];
+  // loop goes through from 0 to user number, adds value by 1 until limit
   for (var i = 0; i <= userInput; i++) {
-// checks for input divisible by "3"
+    // checks for input divisible by "3"
     if  (i > 0 && i % 3 === 0) {
       answer.push (" I'm sorry, Dave. I'm afraid I can't do that.");
-// Turns input into string, and checks for elements containing "0" or "1".
+      // Turns input into string, and checks for elements containing "0" or "1".
     } else if (i.toString().includes("0")) {
       answer.push(" Beep!");
     } else if (i.toString().includes("1")) {
       answer.push(" Boop!");
     } else {
       answer.push(i + "");
-  };
-};
-// console.log(answer)
-return answer;
+    }
+  }
+  return answer;
 };
 
 //Front End User Interface
 
 $(document).ready(function() {
   $("form#userForm").submit(function(event) {
-  event.preventDefault();
-  var inputNumber = parseInt($("input#inputUserForm").val());
-  var finalNumber = beepBoop(inputNumber);
+    event.preventDefault();
+    var inputNumber = parseInt($("input#inputUserForm").val());
+    var finalNumber = beepBoop(inputNumber);
 
     if (isNaN(inputNumber)) {
       alert("Error. Not a valid number, try again!");
     }
-// shows results and clears results after new submit
-      $("#answer").show();
-      $("#answer").empty();
+    // shows results and clears results after new submit
+    $("#answer").show();
+    $("#answer").empty();
 
-      $("<li>" + finalNumber + "</li>").appendTo("#answer");
-      $("form#userForm")[0].reset();
+    $("<li>" + finalNumber + "</li>").appendTo("#answer");
+    $("form#userForm")[0].reset();
   });
 });
